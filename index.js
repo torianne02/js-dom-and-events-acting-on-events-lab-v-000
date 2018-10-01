196cd46da7c16a11169c71cc8e31454465e308a9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-01 23:22:59 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let newEmployee = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${newEmployee}</li>`);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event) {
+    addNewElementAsLi();
+    resetInput();
+  });
+}
+
+function resetInput() {
+  document.querySelector('input').value = '';
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+
+  link.addEventListener('click', function(event) {
+    ul.innerHTML = '';
+  });
+}
